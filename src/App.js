7c8d4957fc5916f36d951378f82381f02adf76f7
Alexander Mezentsev <mezentsev20001028@gmail.com>
2023-07-13 20:44:42 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { create } from 'zustand'
+import app from './app.module.css'
+import React, { useState, useEffect } from 'react';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"));
+const useStore = create((set) => ({
+  data: [],
+  fetchData: async () => {
+    const response = await fetch('https://api.punkapi.com/v2/beers?page=1')
+    const dataR = await response.json()
+    set({ data: dataR })
+  },
+}));
 
 function App() {
+  const fetchData = useStore((state) => state.fetchData)
+  const data = useStore((state) => state.data)
+  useEffect(() => {
+    fetchData();
+  }, [] );
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData}>Submit</button>
+      {data.map(el=>{
+        return <div className={app.back}>
+<BrowserRouter> 
+    { <Routes>
+        <Route path="/email" /> 
+      </Routes> } 
+  </BrowserRouter>  
+          <h3>Name</h3>
+          {el.name}
+          <h3>Food pairing</h3>
+          {el.food_pairing}
+          <h3>Id</h3>
+          {el.id}
+        </div>
+        })}
     </div>
   );
 }
