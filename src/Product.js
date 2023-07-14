259bac3c98efe408a27fd22f693288feccb28d2d
Alexander Mezentsev <mezentsev20001028@gmail.com>
2023-07-14 11:08:34 +0300

    Map commit

--- src/Product.js
+++ src/Product.js
@@ -15,13 +15,31 @@ function Product() {
         {
            data.map(el=>{
              return <div className={app.back}>
-            <h3>Name</h3>
+            <h4>Name</h4>
             {el.name}
-            <h3>Food pairing</h3>
+            <h4>Food pairing</h4>
             {el.food_pairing}
-            <h3>Id</h3>
+            <h4>Id</h4>
             <Link to={'/' + el.id} >details</Link>
             {el.id}
+            <h4>Tagline</h4>
+            {el.tagline}
+            <h4>First brewed</h4>
+            {el.first_brewed}
+            <h4>Description</h4>
+            {el.description}
+            <h4>{el.image_url}</h4>
+            <h4>Abv</h4>
+            {el.abv}
+            <h4>Ibu</h4>
+            {el.ibu}
+            <h4>Target FG</h4>
+            {el.target_fg}
+            <h4>Target FG</h4>
+            {el.target_fg}
+            <h4>Ebc</h4>
+            {el.ebc}
+
           </div>
           })
         }
